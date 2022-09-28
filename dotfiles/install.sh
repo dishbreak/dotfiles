e6f0bf1d33c5ae5f8eef32fb898cff4687dabc7d
Vishal Kotcherlakota <dishbreak@gmail.com>
2022-09-27 22:20:35 -0700

    make the script re-runnable

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -11,7 +11,6 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 echo ".cfg" >> .gitignore
 
 CONF_DIR="$HOME/.cfg"
-
 if [[ -d "$CONF_DIR" ]]; then
     echo "removing prior config dir"
     rm -rf "$CONF_DIR"
@@ -21,9 +20,19 @@ git clone --bare git@github.com:dishbreak/dotfiles.git "$CONF_DIR"
 
 config checkout -f
 
-echo "installing zshrc"
+OMZ_DIR="$HOME/.oh-my-zsh"
+if [[ -d "$OMZ_DIR" ]]; then 
+    echo "removing oh-my-zsh dir"
+    rm -rf "$OMZ_DIR"
+fi
+echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 
+SCMB_DIR="$HOME/.scm_breeze"
+if [[ -d "$SCMB_DIR"]]; then
+    echo "removing scm breeze dir"
+    rm -rf "$SCMB_DIR"
+fi
 echo "install scm_breeze"
 git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
