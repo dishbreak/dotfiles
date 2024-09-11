06637960d9a91d8f28aa8d6eb1db14350f67efa3
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2024-09-11 00:40:47 -0700

    Update install.sh
    drop the bullettrain theme stuff

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -61,10 +61,6 @@ echo "installing oh-my-zsh"
 export RUNZSH=no # don't exec ZSH in the installer pls
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 
-BULLETTRAIN_THEME_PATH="$OMZ_DIR/themes/bullet-train.zsh-theme"
-curt "http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme" -o "$BULLETTRAIN_THEME_PATH"
-chmod 655 "$BULLETTRAIN_THEME_PATH"
-
 SCMB_DIR="$HOME/.scm_breeze"
 if [[ -d "$SCMB_DIR"]]; then
     echo "removing scm breeze dir"
